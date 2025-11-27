03026c3f33bec08cc795f41bbaf8f434aba66f81
Alex Hayter <holcroft.house@gmail.com>
2025-11-27 20:14:25 +0000

    semaphore tutorial

--- src/main.c
+++ src/main.c
@@ -3,34 +3,45 @@
 #include <stdio.h>
 #include <queue.h>
 #include "pico/stdlib.h"
+#include "semphr.h"
 
-static QueueHandle_t xQueue = NULL;
+static SemaphoreHandle_t mutex;
 
-void task1(void *pvParameters)
+void task1()
 {
-    while (true){
-        printf("Task 1 is currently running\n");
-        //for(int i=0;i<20000000;i++){};
-        vTaskDelay(100);
+   char ch = '1';
+   while(true){
+    if(xSemaphoreTake(mutex,0) == pdTRUE){
+        for(int i=1;i<10;i++){
+            putchar(ch);
+        }
+        puts("");
+        xSemaphoreGive(mutex);
     }
+   }
 }
-void task2(void *pvParameters)
+void task2()
 {
-    while (true){
-        printf("Task 2 is currently running\n");
-        //for(int i=0;i<20000000;i++){};
-        vTaskDelay(100);
+    char ch = '2';
+   while(true){
+    if(xSemaphoreTake(mutex,0) == pdTRUE){
+        for(int i=1;i<10;i++){
+            putchar(ch);
     }
+    puts("");
+    xSemaphoreGive(mutex);
+    }
+   }
 }
 
 int main()
 {
     stdio_init_all();
 
-    xQueue = xQueueCreate(1,sizeof(uint));
+    mutex = xSemaphoreCreateMutex();
 
     xTaskCreate(task1,"Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(task2,"Task 2", 256, NULL, 2, NULL);
+    xTaskCreate(task2,"Task 2", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
