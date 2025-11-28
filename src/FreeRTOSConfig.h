e0eff679bd7621fe41094740d35397cb285b21d6
Alex Hayter <holcroft.house@gmail.com>
2025-11-28 18:27:58 +0000

    now

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -45,16 +45,16 @@
 #define configUSE_TICKLESS_IDLE                 0
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
-#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES                    32
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 100000 )
+#define configMAX_PRIORITIES                    5
 #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 256
 #define configUSE_16_BIT_TICKS                  0
 
-#define configIDLE_SHOULD_YIELD                 1
+#define configIDLE_SHOULD_YIELD                 0
 
 /* Synchronization Related */
 #define configUSE_MUTEXES                       1
-#define configUSE_RECURSIVE_MUTEXES             1
+#define configUSE_RECURSIVE_MUTEXES             0
 #define configUSE_APPLICATION_TASK_TAG          0
 #define configUSE_COUNTING_SEMAPHORES           1
 #define configQUEUE_REGISTRY_SIZE               8
