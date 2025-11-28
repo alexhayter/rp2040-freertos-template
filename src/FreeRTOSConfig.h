85d3c6183171f8587f39f0a14890b667d08624c6
Alex Hayter <holcroft.house@gmail.com>
2025-11-28 21:00:18 +0000

    working  counting semaphore

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -45,7 +45,7 @@
 #define configUSE_TICKLESS_IDLE                 0
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
-#define configTICK_RATE_HZ                      ( ( TickType_t ) 100000 )
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 100 )
 #define configMAX_PRIORITIES                    5
 #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 256
 #define configUSE_16_BIT_TICKS                  0
--- src/main.c
+++ src/main.c
@@ -4,44 +4,47 @@
 #include <queue.h>
 #include "pico/stdlib.h"
 #include "semphr.h"
+SemaphoreHandle_t count;
 
-static SemaphoreHandle_t mutex;
-
-void task1()
+void LED_task()
 {
-   char ch = '1';
+   const uint LED_PIN = 25;
+   gpio_init(LED_PIN);
+   gpio_set_dir(LED_PIN,GPIO_OUT);
    while(true){
-    if(xSemaphoreTake(mutex,0) == pdTRUE){
-        for(int i=1;i<10;i++){
-            putchar(ch);
-        }
-        puts("");
-        xSemaphoreGive(mutex);
+    if(xSemaphoreTake(count,(TickType_t) 10 ) == pdTRUE){
+        gpio_put(LED_PIN,1);
+        vTaskDelay(100);
+    }
+    else{
+        gpio_put(LED_PIN,0);
     }
    }
 }
-void task2()
-{
-    char ch = '2';
-   while(true){
-    if(xSemaphoreTake(mutex,0) == pdTRUE){
-        for(int i=1;i<10;i++){
-            putchar(ch);
+void button_task(){
+    gpio_init(20);
+    gpio_set_dir(20,GPIO_IN);
+    gpio_pull_up(20);
+
+    while(true){
+        if (gpio_get(20) == 0){
+            xSemaphoreGive(count);
+            vTaskDelay(20);
+        }
+        else{
+            vTaskDelay(1);
+        }
     }
-    puts("");
-    xSemaphoreGive(mutex);
-    }
-   }
 }
 
 int main()
 {
     stdio_init_all();
 
-    mutex = xSemaphoreCreateMutex();
+    count= xSemaphoreCreateCounting(5,0);
 
-    xTaskCreate(task1,"Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(task2,"Task 2", 256, NULL, 1, NULL);
+    xTaskCreate(LED_task,"LED Task", 256, NULL, 1, NULL);
+    xTaskCreate(button_task,"Button_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
